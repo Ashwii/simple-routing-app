57e14ef7a10c7ad8b4479df2166a115769b3e724
Ashwii <ashwinianandhraj@gmail.com>
2018-03-17 20:50:55 +0530

    Partial Templates added

--- Simple-Routing-App/simple-routing-app.js
+++ Simple-Routing-App/simple-routing-app.js
@@ -1,41 +1,41 @@
 //Create the module and configure Routing
 //.......................................
-var app = angular.module("routingApp",["ngRoute"]);
+var app = angular.module("routingApp",["ngRoute"])
 //$routeProvider to configure different toutes in our appliction
 				 .config(function($routeProvider){
 					$routeProvider
 						.when("/home",{
-							templeteUrl: "home.html";
-							controller: "homeCntr";
-						}),
+							templateUrl: "Partial-temp/home.html",
+							controller: "homeCntr"
+						})
 						.when("/classes",{
-							templeteUrl: "classes.html";
-							controller: "classesCntr";
-						}),
+							templateUrl: "Partial-temp/classes.html",
+							controller: "classesCntr"
+						})
 						.when("/studentList",{
-							templeteUrl: "studentList.html";
-							controller: "studentListCntr";
-						}),
+							templateUrl: "Partial-temp/studentList.html",
+							controller: "studentListCntr"
+						})
 						
 						.when("/about-us",{
-							templeteUrl: "about-us.html";
-							controller: "aboutCntr";
-						}),
-						.when("/contact",{
-							templeteUrl: "contact.html";
-							controller: "contactCntr";
+							templateUrl: "Partial-temp/about-us.html",
+							controller: "aboutCntr"
 						})
-});
-		  app.controller("homeCntr",function($scope){
-		  			$scope.message = "Home Page";
-		  });
-		  app.controller("classesCntr",function($scope){
-		  	var classes = ["1st standard","2nd standard","3rd standard",
+						.when("/contact",{
+							templateUrl: "Partial-temp/contact.html",
+							controller: "contactCntr"
+						})
+					})
+		  		.controller("homeCntr",function($scope){
+		  			$scope.message = "Krishna Vidyalaya";
+		  })
+		  		.controller("classesCntr",function($scope){
+		  			var classes = ["1st standard","2nd standard","3rd standard",
 		  					"4th standard","5th standard"];
-		  		$scope.classes = classes;
-		  });
-		  app.controller("studentListCntr",function($scope){
-		  	var students = [{Name:"Ashwini",Gender:"Female",Age:22},
+		  			$scope.classes = classes;
+		  })
+		  		.controller("studentListCntr",function($scope){
+		  			var students = [{Name:"Ashwini",Gender:"Female",Age:22},
 		  					{Name:"Kavya",Gender:"Female",Age:09},
 		  					{Name:"Ashok",Gender:"Male",Age:12},
 		  					{Name:"Tamil",Gender:"Male",Age:15},
@@ -45,3 +45,4 @@ var app = angular.module("routingApp",["ngRoute"]);
 		  	$scope.students = students;
 		  });
 		  
+
